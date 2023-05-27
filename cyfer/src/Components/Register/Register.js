875afb6ac7272f4a40e9a17888a982b7e8f59d7f
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-27 21:37:34 +0800

    Update Register to import axios

--- cyfer/src/Components/Register/Register.js
+++ cyfer/src/Components/Register/Register.js
@@ -5,6 +5,7 @@ import {
     faInfoCircle,
 } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "./api/axios";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
