29b1ec77f4baad0cfb5e8bc0d2d511f3c25e3f25
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-28 12:37:18 +0800

    created side nav top nav and layout component
    added heroicons as well

--- cyfer/src/Pages/index.js
+++ cyfer/src/Pages/index.js
@@ -2,14 +2,14 @@ import Head from "next/head";
 // import { subDays, subHours } from 'date-fns';
 import { Box, Container, Unstable_Grid2 as Grid } from "@mui/material";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
-import { OverviewBudget } from "src/sections/overview/overview-budget";
-import { OverviewLatestOrders } from "src/sections/overview/overview-latest-orders";
-import { OverviewLatestProducts } from "src/sections/overview/overview-latest-products";
-import { OverviewSales } from "src/sections/overview/overview-sales";
-import { OverviewTasksProgress } from "src/sections/overview/overview-tasks-progress";
-import { OverviewTotalCustomers } from "src/sections/overview/overview-total-customers";
-import { OverviewTotalProfit } from "src/sections/overview/overview-total-profit";
-import { OverviewTraffic } from "src/sections/overview/overview-traffic";
+// import { OverviewBudget } from "src/sections/overview/overview-budget";
+//import { OverviewLatestOrders } from "src/sections/overview/overview-latest-orders";
+//import { OverviewLatestProducts } from "src/sections/overview/overview-latest-products";
+//import { OverviewSales } from "src/sections/overview/overview-sales";
+//import { OverviewTasksProgress } from "src/sections/overview/overview-tasks-progress";
+//import { OverviewTotalCustomers } from "src/sections/overview/overview-total-customers";
+//import { OverviewTotalProfit } from "src/sections/overview/overview-total-profit";
+//import { OverviewTraffic } from "src/sections/overview/overview-traffic";
 
 const now = new Date();
 
@@ -28,35 +28,35 @@ const Page = () => (
             <Container maxWidth="xl">
                 <Grid container spacing={3}>
                     <Grid xs={12} sm={6} lg={3}>
-                        <OverviewBudget
+                        {/* <OverviewBudget
                             difference={12}
                             positive
                             sx={{ height: "100%" }}
                             value="$24k"
-                        />
+                        /> */}
                     </Grid>
                     <Grid xs={12} sm={6} lg={3}>
-                        <OverviewTotalCustomers
+                        {/* <OverviewTotalCustomers
                             difference={16}
                             positive={false}
                             sx={{ height: "100%" }}
                             value="1.6k"
-                        />
+                        /> */}
                     </Grid>
                     <Grid xs={12} sm={6} lg={3}>
-                        <OverviewTasksProgress
+                        {/* <OverviewTasksProgress
                             sx={{ height: "100%" }}
                             value={75.5}
-                        />
+                        /> */}
                     </Grid>
                     <Grid xs={12} sm={6} lg={3}>
-                        <OverviewTotalProfit
+                        {/* <OverviewTotalProfit
                             sx={{ height: "100%" }}
                             value="$15k"
-                        />
+                        /> */}
                     </Grid>
                     <Grid xs={12} lg={8}>
-                        <OverviewSales
+                        {/* <OverviewSales
                             chartSeries={[
                                 {
                                     name: "This year",
@@ -74,17 +74,17 @@ const Page = () => (
                                 },
                             ]}
                             sx={{ height: "100%" }}
-                        />
+                        /> */}
                     </Grid>
                     <Grid xs={12} md={6} lg={4}>
-                        <OverviewTraffic
+                        {/* <OverviewTraffic
                             chartSeries={[63, 15, 22]}
                             labels={["Desktop", "Tablet", "Phone"]}
                             sx={{ height: "100%" }}
-                        />
+                        /> */}
                     </Grid>
                     <Grid xs={12} md={6} lg={4}>
-                        <OverviewLatestProducts
+                        {/* <OverviewLatestProducts
                             products={[
                                 {
                                     id: "5ece2c077e39da27658aa8a9",
@@ -130,10 +130,10 @@ const Page = () => (
                                 },
                             ]}
                             sx={{ height: "100%" }}
-                        />
+                        /> */}
                     </Grid>
                     <Grid xs={12} md={12} lg={8}>
-                        <OverviewLatestOrders
+                        {/* <OverviewLatestOrders
                             orders={[
                                 {
                                     id: "f69f88012978187a6c12897f",
@@ -197,7 +197,7 @@ const Page = () => (
                                 },
                             ]}
                             sx={{ height: "100%" }}
-                        />
+                        /> */}
                     </Grid>
                 </Grid>
             </Container>
