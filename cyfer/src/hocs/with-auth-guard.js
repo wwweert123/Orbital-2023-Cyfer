9019e34a552a97ab60b59eac0dd27e4d8b30aa6c
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-28 12:54:22 +0800

    added the dashboard page from template

---
+++ cyfer/src/hocs/with-auth-guard.js
@@ -0,0 +1,8 @@
+import { AuthGuard } from "src/guards/auth-guard";
+
+export const withAuthGuard = (Component) => (props) =>
+    (
+        <AuthGuard>
+            <Component {...props} />
+        </AuthGuard>
+    );
