62d449f1c2cf8c2285acc9f8d33d4a733a94f419
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-28 16:34:20 +0800

    Added some routes and useAuth

--- cyfer/src/App.js
+++ cyfer/src/App.js
@@ -1,15 +1,36 @@
 import Login from "./Components/LoginComponent/Login";
 import Register from "./Components/Register/Register";
+import { Routes, Route } from "react-router-dom";
 
 import { useState } from "react";
+import Layout from "./Components/Layout";
+import Missing from "./Components/Missing";
+import Unauthorized from "./Components/Unauthorized";
+import Home from "./Components/Home";
 
 function App() {
-    const [register, setRegister] = useState(false);
+    // const [register, setRegister] = useState(false);
     return (
-        <main>
-            {register ? <Register showRegister={setRegister} /> : null};
-            {!register ? <Login showRegister={setRegister} /> : null};
-        </main>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                {/* public routes */}
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="unauthorized" element={<Unauthorized />} />
+
+                {/* we want to protect these routes */}
+                <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+                    <Route path="/" element={<Home />} />
+                </Route>
+
+                <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+                    <Route path="admin" element={<Admin />} />
+                </Route>
+
+                {/* catch all */}
+                <Route path="*" element={<Missing />} />
+            </Route>
+        </Routes>
     );
 }
 
