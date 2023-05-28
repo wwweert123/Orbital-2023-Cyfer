2488337b1ae184ce4c210005fbc2b5d5666ee4a7
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-28 20:54:29 +0800

    Update axios.js

--- cyfer/src/api/axios.js
+++ cyfer/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://cyfer-backend-orbital.herokuapp.com/",
+    baseURL: "https://cyfer-backend-orbital.herokuapp.com",
 });
