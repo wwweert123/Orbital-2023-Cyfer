0dbb1170b2c74b84233ca50eaf34d11d3a9989d3
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-28 13:02:56 +0800

    Revert "added the dashboard page from template"
    This reverts commit 9019e34a552a97ab60b59eac0dd27e4d8b30aa6c.

--- cyfer/src/App.js
+++ cyfer/src/App.js
@@ -2,13 +2,11 @@ import Login from "./Components/LoginComponent/Login";
 import Register from "./Components/Register/Register";
 
 import { useState } from "react";
-import Page from "./Pages/index";
 
 function App() {
     const [register, setRegister] = useState(false);
     return (
         <main>
-            <Page />
             {register ? <Register showRegister={setRegister} /> : null};
             {!register ? <Login showRegister={setRegister} /> : null};
         </main>
