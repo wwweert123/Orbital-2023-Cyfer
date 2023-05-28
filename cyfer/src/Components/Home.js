7fc9bffe7ee34d165ca6d713f159976b11c4f452
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-28 22:06:39 +0800

    when log out
    navigate to login page

--- cyfer/src/Components/Home.js
+++ cyfer/src/Components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
         // if used in more components, this should be in context
         // axios to /logout endpoint
         setAuth({});
-        navigate("/linkpage");
+        navigate("/login");
     };
 
     return (
