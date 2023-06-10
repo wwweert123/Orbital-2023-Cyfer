1180ee60a95967c370f6bf7eb3e5d9b97c7abe69
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-10 22:19:44 +0800

    Include the hook for persistLogin

--- cyfer/src/App.js
+++ cyfer/src/App.js
@@ -8,6 +8,7 @@ import Unauthorized from "./Components/Unauthorized";
 import Home from "./Components/Home";
 import Admin from "./Components/Admin";
 import RequireAuth from "./Components/RequireAuth";
+import PersistLogin from "./Components/PersistLogin";
 
 const ROLES = {
     User: 2001,
@@ -26,14 +27,19 @@ function App() {
                 <Route path="unauthorized" element={<Unauthorized />} />
 
                 {/* we want to protect these routes */}
-                <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-                    <Route path="/" element={<Home />} />
-                </Route>
+                <Route element={<PersistLogin />}>
+                    <Route
+                        element={<RequireAuth allowedRoles={[ROLES.User]} />}
+                    >
+                        <Route path="/" element={<Home />} />
+                    </Route>
 
-                <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
-                    <Route path="admin" element={<Admin />} />
+                    <Route
+                        element={<RequireAuth allowedRoles={[ROLES.Admin]} />}
+                    >
+                        <Route path="admin" element={<Admin />} />
+                    </Route>
                 </Route>
-
                 {/* catch all */}
                 <Route path="*" element={<Missing />} />
             </Route>
