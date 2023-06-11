e08df4bf001616c95fe569c88a1ad8b0a70591e5
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-11 13:41:26 +0800

    created the reusable logic

--- cyfer/src/hooks/useLocalStorage.js
+++ cyfer/src/hooks/useLocalStorage.js
@@ -1,10 +1,25 @@
 import { useState, useEffect } from "react";
 
+const getLocalValue = (key, initValue) => {
+    //SSR Next.js
+    if (typeof window === "undefined") {
+        return initValue;
+    }
+    //if a value is already stored
+    const localValue = JSON.parse(localStorage.getItem(key));
+    if (localValue) return localValue;
+
+    //return result of a a function
+    if (initValue instanceof Function) return initValue();
+
+    return initValue;
+};
+
 // use this function to get and set values from local storage
 const useLocalStorage = (key, initValue) => {
-    const [value, setValue] = useState(
-        JSON.parse(localStorage.getItem(key)) || initValue
-    );
+    const [value, setValue] = useState(() => {
+        return getLocalValue(key, initValue);
+    });
 
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value));
