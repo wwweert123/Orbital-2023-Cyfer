b2d9c7389150581b401b6a6c686493cf96fee125
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-11 14:19:35 +0800

    remove password from auth state

--- cyfer/src/Components/LoginComponent/Login.jsx
+++ cyfer/src/Components/LoginComponent/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
             console.log(JSON.stringify(response?.data));
             const accessToken = response?.data?.accessToken;
             const roles = response?.data?.roles;
-            setAuth({ user, pwd, roles, accessToken });
+            setAuth({ user, roles, accessToken });
             //setUser("");
             resetUser();
             setPwd("");
