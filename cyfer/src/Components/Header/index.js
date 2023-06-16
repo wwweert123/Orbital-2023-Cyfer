9766db510f8d49ed15f9e830cc5d297213e44d67
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-16 13:50:55 +0800

    removed notifications popover
    added the scrollbar component

--- cyfer/src/Components/Header/index.js
+++ cyfer/src/Components/Header/index.js
@@ -59,7 +59,6 @@ export default function Header({ onOpenNav }) {
                     }}
                 >
                     <LanguagePopover />
-                    {/*<NotificationsPopover />*/}
                     <AccountPopover />
                 </Stack>
             </StyledToolbar>
