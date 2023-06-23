ed8e2bd9dbd4aef190b05fee3ab856fd0275873f
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-23 23:53:33 +0800

    added the view page

---
+++ cyfer/src/Components/Wallets.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import useAxiosPrivate from "../hooks/useAxiosPrivate"; //import the hook
+import { useNavigate, useLocation } from "react-router-dom";
+import { Grid, Stack, Typography } from "@mui/material";
+
+import ContractWidgetSummary from "../sections/viewsections/ContractWidgetSummary";
+import walletShort from "wallet-short";
+
+const Wallets = () => {
+    const [walletsObjects, setWalletsObjects] = useState([]);
+    const axiosPrivate = useAxiosPrivate();
+    const navigate = useNavigate();
+    const location = useLocation(); //current location
+
+    useEffect(() => {
+        let isMounted = true;
+        const controller = new AbortController(); // cancel any pending request if the component unmounts
+
+        const getWallets = async () => {
+            try {
+                const response = await axiosPrivate.get("/wallet", {
+                    signal: controller.signal,
+                });
+                console.log(response.data);
+                isMounted && setWalletsObjects(response.data);
+            } catch (err) {
+                console.error(err);
+                navigate("/login", {
+                    state: { from: location },
+                    replace: true,
+                });
+            }
+        };
+        getWallets();
+        return () => {
+            isMounted = false;
+            controller.abort(); // abort request
+        };
+        // return statement performs the cleanup when the component unmount or after the previous render
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const walletItems = walletsObjects.map((wallet, i) => (
+        <Stack spacing={3} id={i}>
+            <Typography variant="h3">{wallet.address}</Typography>
+            <Grid container spacing={3}>
+                {wallet.owned.map((address) => (
+                    <Grid item xs={12} sm={6} md={3}>
+                        <ContractWidgetSummary
+                            title="Weekly Sales"
+                            address={walletShort(address)}
+                            icon={"ant-design:android-filled"}
+                        />
+                    </Grid>
+                ))}
+                {wallet.editor.map((address) => (
+                    <Grid item xs={12} sm={6} md={3}>
+                        <ContractWidgetSummary
+                            title="Weekly Sales"
+                            address={walletShort(address)}
+                            icon={"ant-design:android-filled"}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+        </Stack>
+    ));
+
+    return (
+        <>
+            {walletsObjects?.length ? (
+                <Stack spacing={3}>{walletItems}</Stack>
+            ) : (
+                " "
+            )}
+        </>
+    );
+};
+
+export default Wallets;
--- cyfer/src/Pages/ViewPage.js
+++ cyfer/src/Pages/ViewPage.js
@@ -1,12 +1,11 @@
 import { Helmet } from "react-helmet-async";
 // @mui
 //import { useTheme } from "@mui/material/styles";
-import { Grid, Container, Typography } from "@mui/material";
-import ContractWidgetSummary from "../sections/viewsections/ContractWidgetSummary";
+import { Container, Typography } from "@mui/material";
+import Wallets from "../Components/Wallets";
 
 export default function ViewPage() {
     //const theme = useTheme();
-
     return (
         <>
             <Helmet>
@@ -17,51 +16,7 @@ export default function ViewPage() {
                 <Typography variant="h4" sx={{ mb: 5 }}>
                     View your Contracts here
                 </Typography>
-
-                <Grid container spacing={3}>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <ContractWidgetSummary
-                            title="Weekly Sales"
-                            address="714K"
-                            icon={"ant-design:android-filled"}
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <ContractWidgetSummary
-                            title="Weekly Sales"
-                            address="714K"
-                            icon={"ant-design:android-filled"}
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <ContractWidgetSummary
-                            title="Weekly Sales"
-                            address="714K"
-                            icon={"ant-design:android-filled"}
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <ContractWidgetSummary
-                            title="Weekly Sales"
-                            address="714K"
-                            icon={"ant-design:android-filled"}
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <ContractWidgetSummary
-                            title="Weekly Sales"
-                            address="714K"
-                            icon={"ant-design:android-filled"}
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <ContractWidgetSummary
-                            title="Weekly Sales"
-                            address="714K"
-                            icon={"ant-design:android-filled"}
-                        />
-                    </Grid>
-                </Grid>
+                <Wallets />
             </Container>
         </>
     );
