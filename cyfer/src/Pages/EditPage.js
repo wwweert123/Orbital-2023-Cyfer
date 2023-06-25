177646f16feb8bcc589d72278e1b31f46cf0eb32
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-25 15:55:05 +0800

    Updated the edit page

--- cyfer/src/Pages/EditPage.js
+++ cyfer/src/Pages/EditPage.js
@@ -2,11 +2,27 @@ import { Helmet } from "react-helmet-async";
 
 import useWallet from "../hooks/useWallet";
 
-// @mui
-import { Container, Typography } from "@mui/material";
+import { useState } from "react";
 
-export default function ViewPage() {
+// @mui
+import {
+    Container,
+    Typography,
+    Box,
+    InputLabel,
+    MenuItem,
+    FormControl,
+    Select,
+} from "@mui/material";
+
+export default function EditPage() {
     const { wallet } = useWallet();
+
+    const [contract, setContract] = useState("");
+
+    const handleChange = (e) => {
+        setContract(e.target.value);
+    };
     //const theme = useTheme();
     return (
         <>
@@ -18,9 +34,25 @@ export default function ViewPage() {
                 <Typography variant="h4" sx={{ mb: 5 }}>
                     Edit the clauses for your contract here
                 </Typography>
-                <Typography variant="h5" sx={{ mb: 5 }}>
-                    Your selected wallet is :{wallet}
-                </Typography>
+                <Box>
+                    <Typography variant="h5" sx={{ mb: 5 }}>
+                        Your selected wallet is :{wallet}
+                    </Typography>
+                    <FormControl fullWidth>
+                        <InputLabel id="contract-select-label">Age</InputLabel>
+                        <Select
+                            labelId="contract-select-label"
+                            id="contract-select"
+                            value={contract}
+                            label="Contract"
+                            onChange={handleChange}
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Box>
             </Container>
         </>
     );
