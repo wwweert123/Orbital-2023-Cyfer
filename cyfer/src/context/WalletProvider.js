da76fcaf1dd4e9d4b65bed748d3b5b1b2dc524e6
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-25 12:54:35 +0800

    Create WalletProvider.js

---
+++ cyfer/src/context/WalletProvider.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+const WalletContext = createContext({});
+
+export const WalletProvider = ({ children }) => {
+    const [wallet, setWallet] = useState({});
+    // const [persist, setPersist] = useState(
+    //     JSON.parse(localStorage.getItem("persist")) || false
+    // );
+    return (
+        <WalletContext.Provider value={{ wallet, setWallet }}>
+            {children}
+        </WalletContext.Provider>
+    );
+};
+
+export default WalletContext;
