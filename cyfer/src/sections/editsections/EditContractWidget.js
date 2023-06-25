f4037c5ad0ce2cb2c4a5067d5893565bb465662a
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-25 21:15:42 +0800

    adsfads

--- cyfer/src/sections/editsections/EditContractWidget.js
+++ cyfer/src/sections/editsections/EditContractWidget.js
@@ -68,7 +68,7 @@ export default function EditContractWidget({
             <TextField
                 variant="outlined"
                 multiline
-                sx={{ width: "100%" }}
+                sx={{ width: "100%", m: 1, backgroundColor: "black" }}
                 type="text"
                 id="clause"
                 name="clause"
