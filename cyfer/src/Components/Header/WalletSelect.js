6ad365384b415c9b666a4c9c2535b40d62a56265
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-30 10:38:14 +0800

    Update WalletSelect.js

--- cyfer/src/Components/Header/WalletSelect.js
+++ cyfer/src/Components/Header/WalletSelect.js
@@ -62,7 +62,7 @@ export default function WalletSelect({ color = "primary" }) {
                 isMounted && setWallets(walletAddresses);
             } catch (err) {
                 console.error(err);
-                navigate("/", {
+                navigate("/login", {
                     state: { from: location },
                     replace: true,
                 });
