5ddb96f76528c727987e343c6a98043996d0c5de
Frederick Pua <puachengxuan.fp@gmail.com>
2023-07-10 14:17:17 +0800

    improved the css of the steppers

--- cyfer/src/sections/createsections/CreateSteps.js
+++ cyfer/src/sections/createsections/CreateSteps.js
@@ -7,7 +7,16 @@ import {
     Button,
     Typography,
 } from "@mui/material";
-import { alignProperty } from "@mui/material/styles/cssUtils";
+import StepConnector, {
+    stepConnectorClasses,
+} from "@mui/material/StepConnector";
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
+import {
+    Settings as SettingsIcon,
+    GroupAdd as GroupAddIcon,
+    VideoLabel as VideoLabelIcon,
+} from "@mui/icons-material";
 
 import { useState } from "react";
 
@@ -17,6 +26,90 @@ const steps = [
     "Create the contract",
     "Add editors",
 ];
+
+const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
+    [`&.${stepConnectorClasses.alternativeLabel}`]: {
+        top: 22,
+    },
+    [`&.${stepConnectorClasses.active}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+            backgroundImage:
+                "linear-gradient(90deg, rgba(104,3,128,1) 0%, rgba(38,19,184,0.9977240896358543) 35%, rgba(0,138,255,1) 100%)",
+        },
+    },
+    [`&.${stepConnectorClasses.completed}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+            backgroundImage:
+                "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)",
+        },
+    },
+    [`& .${stepConnectorClasses.line}`]: {
+        height: 3,
+        border: 0,
+        backgroundColor:
+            theme.palette.mode === "dark" ? theme.palette.grey[800] : "#eaeaf0",
+        borderRadius: 1,
+    },
+}));
+
+const ColorlibStepIconRoot = styled("div")(({ theme, ownerState }) => ({
+    backgroundColor:
+        theme.palette.mode === "dark" ? theme.palette.grey[700] : "#ccc",
+    zIndex: 1,
+    color: "#fff",
+    width: 50,
+    height: 50,
+    display: "flex",
+    borderRadius: "50%",
+    justifyContent: "center",
+    alignItems: "center",
+    ...(ownerState.active && {
+        backgroundImage:
+            "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+        boxShadow: "0 4px 10px 0 rgba(0,0,0,.25)",
+    }),
+    ...(ownerState.completed && {
+        backgroundImage:
+            "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+    }),
+}));
+
+function ColorlibStepIcon(props) {
+    const { active, completed, className } = props;
+
+    const icons = {
+        1: <SettingsIcon />,
+        2: <GroupAddIcon />,
+        3: <VideoLabelIcon />,
+    };
+    console.log(props.icon);
+    return (
+        <ColorlibStepIconRoot
+            ownerState={{ completed, active }}
+            className={className}
+        >
+            {icons[String(props.icon)]}
+        </ColorlibStepIconRoot>
+    );
+}
+
+ColorlibStepIcon.propTypes = {
+    /**
+     * Whether this step is active.
+     * @default false
+     */
+    active: PropTypes.bool,
+    className: PropTypes.string,
+    /**
+     * Mark the step as completed. Is passed to child components.
+     * @default false
+     */
+    completed: PropTypes.bool,
+    /**
+     * The label displayed in the step icon.
+     */
+    icon: PropTypes.node,
+};
 
 export default function CreateSteps() {
     const [activeStep, setActiveStep] = useState(0);
@@ -31,13 +124,22 @@ export default function CreateSteps() {
 
     return (
         <Box sx={{ width: "95%", m: "auto" }}>
-            <Stepper activeStep={activeStep} alternativeLabel>
+            <Stepper
+                activeStep={activeStep}
+                alternativeLabel
+                connector={<ColorlibConnector />}
+            >
                 {steps.map((label, index) => {
                     const stepProps = {};
                     const labelProps = {};
                     return (
                         <Step key={label} {...stepProps}>
-                            <StepLabel {...labelProps}>{label}</StepLabel>
+                            <StepLabel
+                                StepIconComponent={ColorlibStepIcon}
+                                {...labelProps}
+                            >
+                                {label}
+                            </StepLabel>
                         </Step>
                     );
                 })}
