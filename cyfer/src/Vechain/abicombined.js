f4946548d43be68da56e4cfe2d758013185ad6a2
Frederick Pua <puachengxuan.fp@gmail.com>
2023-07-12 16:15:32 +0800

    Added the new abis for the new contracts with bytecodes

---
+++ cyfer/src/Vechain/abicombined.js
@@ -0,0 +1,15 @@
+import { ABI1, byteCode1 } from "./abi1";
+import { ABI2, byteCode2 } from "./abi2";
+import { ABI3, byteCode3 } from "./abi3";
+
+export const ABICombined = {
+    1: ABI1,
+    2: ABI2,
+    3: ABI3,
+};
+
+export const byteCodes = {
+    1: byteCode1,
+    2: byteCode2,
+    3: byteCode3,
+};
