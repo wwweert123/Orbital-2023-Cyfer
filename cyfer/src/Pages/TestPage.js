4908c0a926de9257c92f94cccd1d125b3633e969
DavinciDelta <110587475+DavinciDelta@users.noreply.github.com>
2023-07-13 03:02:12 -0400

    Update contract history

--- cyfer/src/Pages/TestPage.js
+++ cyfer/src/Pages/TestPage.js
@@ -1,74 +1,196 @@
-import { Helmet } from "react-helmet-async";
 import { useEffect, useState } from "react";
-// @mui
-//import { useTheme } from "@mui/material/styles";
-// import { Container, Typography, Grid } from "@mui/material";
-import { Typography } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-//import ContractWidgetSummary from "../sections/viewsections/ContractWidgetSummary";
+// mui
+import {
+    Container,
+    Stack,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+    Typography,
+} from "@mui/material";
 
-import Connex from "../api/connex";
-import { ABI } from "../Vechain/abi";
+// Vechain
+import { abiDict } from "../Vechain/abiDict";
 
-// Dialog
-import AddEditorDialog from "../sections/viewsections/AddEditorDialog";
+import { Helmet } from "react-helmet-async";
+
+// Axios
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import useWallet from "../hooks/useWallet";
+function dataShort(data){
+    if (typeof data === "string") {
+        return (
+        `${data}`.slice(0, 10) +
+        "..." +
+        `${data}`.slice(`${data}`.length - 64)
+        );
+    } else {
+        throw new Error("Invalid format: " + typeof data);
+    }
+}
+
+function fromHex(hex) {
+    let str = '';
+    for (let i = 0; i < hex.length; i += 2) {
+        let temp = String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+        str += temp;
+    }
+    return str;
+}
+
+function decode(encoded, functionName) {
+    let strings = [];
+    if(functionName === 'Set Clause(clause number, string)'){
+        let intVariable = (encoded.slice(0, 64));
+        strings.push(parseInt(intVariable, 16));
+        strings.push(', ');
+        let dataStart = parseInt(encoded.slice(64,128),16);
+        let stringVariable = fromHex(encoded.slice(dataStart+64, encoded.length))
+        strings.push(stringVariable);
+    }
+    else if(functionName === 'Add Editor(address)'){
+        strings.push('0x' + encoded.slice(24,64));
+    }
+    else{
+        for (let i = 64; i < encoded.length; i += 64) {
+            let offset = parseInt(encoded.slice(i, i + 64), 16) * 2;
+            let length = parseInt(encoded.slice(offset, offset + 64), 16) * 2;
+            let data = encoded.slice(offset + 64, offset + 64 + length);
+            strings.push(fromHex(data));
+        }
+
+    }
+    return strings;
+}
 
 export default function TestPage() {
-    const [clauseText, setClauseText] = useState("Empty");
-    // const [expandedID, setExpandedID] = useState();
+    const { wallet } = useWallet();
 
-    // const handleExpanded = (id) => {
-    //     setExpandedID(expandedID !== id ? id : false);
-    // };
+    const [transactionDetails, setTransactionDetails] = useState([]);
 
-    const connex = Connex();
+    const [transactionHistoryCount, setTransactionHistoryCount] = useState("");
+
+    const axiosPrivate = useAxiosPrivate();
+
+    const seeContractHistory = async () => {
+        if (wallet === "") {
+            console.log("no wallet selected");
+            return;
+        }
+        try {
+            const resp = await axiosPrivate.get(
+                `/wallet/gettransaction/${wallet}`
+            );
+            console.log(resp.data);
+            setTransactionHistoryCount(resp.data.count);
+            const details = resp.data.txs.map((item) => {
+                let tempDate = (new Date(item.meta.blockTimestamp * 1000));
+                let fullDate = tempDate.toString();
+                let shortDate = tempDate.toLocaleDateString();
+                let functionName, variable;
+                if (item.size > 1000) {
+                    functionName = "Create Contract"
+                    variable = "null"
+                } 
+                else{
+                    let slicedPortion = item.clauses[0].data.slice(2, 10);
+                    if (abiDict.hasOwnProperty(slicedPortion)) {
+                        functionName = abiDict[slicedPortion];
+                        let restOfData = item.clauses[0].data.slice(10);
+                        variable = decode(restOfData, functionName);
+                    } 
+                    else {
+                        functionName = "unknown";
+                        variable = "unknown";
+                    }
+                }
+                return {
+                    date: fullDate,
+                    headerDate: shortDate,
+                    txID: item.txID,
+                    origin: item.origin,
+                    to: item.clauses[0].to,
+                    data: item.clauses[0].data,
+                    size: item.size,
+                    name: functionName,
+                    parameters: variable
+                };
+            });
+            setTransactionDetails(details);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     useEffect(() => {
-        const getClauseText = async () => {
-            console.log("hi");
-            const readABI = ABI.find(({ name }) => name === "retrieve");
-            const result = await connex.thor
-                .account("0x9524bb149161edd41b13039b4ec4d95bc1e23f8b")
-                .method(readABI)
-                .call(2);
-            if (result) {
-                console.log(result);
-                setClauseText(result.decoded[0]);
-            }
-        };
-        getClauseText();
+        seeContractHistory();
         // eslint-disable-next-line
-    }, []);
+    }, [wallet]);
 
-    //const theme = useTheme();
     return (
         <>
             <Helmet>
-                <title> Test | Minimal UI </title>
+                <title> Transaction History</title>
             </Helmet>
-
-            {/*<Container maxWidth="xl">
-                <Typography variant="h4" sx={{ mb: 5 }}>
-                    View your Contracts here
-                </Typography>
-                <Grid container spacing={3}>
-                    <Grid
-                        item
-                        xs={12}
-                        sm={6}
-                        md={expandedID === "hehehe" ? 12 : 3}
-                    >
-                        <ContractWidgetSummary
-                            title="Contract Owner"
-                            address="hehehe"
-                            icon={"fluent-mdl2:party-leader"}
-                            id="hehehe"
-                            handleExpanded={handleExpanded}
-                        />
-                    </Grid>
-                </Grid> */}
-            <Typography>{clauseText}</Typography>
-            <AddEditorDialog />
-            {/* </Container> */}
+            <Container maxWidth="xl">
+                <Stack spacing={3}>
+                    <Typography variant="h4" sx={{ mb: 5 }}>
+                        Transaction History
+                    </Typography>
+                    <Typography variant="h5" sx={{ mb: 5 }}>
+                        Your selected wallet is :{wallet}
+                    </Typography>
+                    <Typography variant="h5">
+                        Number of transactions: {transactionHistoryCount}
+                    </Typography>
+                    <Typography variant="h6">
+                        Recent transactions:
+                    </Typography>
+                    {transactionDetails.map((detail, index) => (
+                        <div key={index}>
+                            <Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1d-content"
+                                    id="panel1d-header"
+                                    sx={{
+                                        backgroundColor: (theme) =>
+                                            theme.palette.background.neutral,
+                                    }}
+                                >
+                                <Typography variant="subtitle2">
+                                    Index:{index} {detail.headerDate}
+                                </Typography>
+                                </AccordionSummary>
+                                <AccordionDetails
+                                    sx={{backgroundColor: (theme) => theme.palette.grey[700],}}
+                                >
+                                    <Typography variant="body1">
+                                        Date: {detail.date}   
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        Transaction ID: {detail.txID}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        Origin: {detail.origin}
+                                    </Typography>
+                                    <Typography>
+                                        To: {detail.to}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        Data: {dataShort(detail.data)}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        Function Name: {detail.name}     Parameters: {detail.parameters}
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                        </div>
+                    ))}
+                </Stack>
+            </Container>
         </>
     );
 }
